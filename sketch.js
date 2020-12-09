61a9bffeb32655a2e0d8e86114c112dfa0dd56ab
sarim26 <71965173+sarim26@users.noreply.github.com>
2020-12-09 16:13:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,83 +1,115 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
-var bird, slingShot;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+var boy, boyImg;
+var tree, treeImg;
+var stone;
+var slingShot;
+var mango1, mango2, mango3, mango4, mango5, mango6;
+
+var slingShot;
+
+function preload()
+{
+  boyImg=loadImage("boy.png")
+  treeImg=loadImage("tree.png")
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+	createCanvas(800, 700);
+
+	boy = createSprite(200,635);
+	boy.addImage(boyImg);
+  boy.scale=0.1;
+
+  tree = createSprite(600,450);
+	tree.addImage(treeImg);
+  tree.scale=0.45;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = new Ground(400,height,800,40);
+
+	stone = new Stone(145,575,40);
+
+	mango1 = new Mango(600, 400, 30);
+	mango2 = new Mango(600, 300, 30);
+	mango3 = new Mango(500, 400, 30);
+	mango4 = new Mango(700, 400, 30);
+	mango5 = new Mango(500, 300, 30);
+	mango6 = new Mango(700, 300, 30);
+
+	slingShot = new slingshot(stone.body, {x: 145, y: 575});
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
-    log6 = new Log(230,180,80, PI/2);
-    slingShot = new Slingshot(bird.body,{x:56,y:78});
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    log6.display();
-    slingShot.display();    
-}
-
-function mouseDragged(){
-
-    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+	Engine.run(engine);
+  
 }
 
 
-function mouseReleased(){
+function draw() {
+  rectMode(CENTER);
+  background("grey");
+  drawSprites();
+  ground.display();
 
-    slingShot.fly()
+  stone.display();
+
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+
+  slingShot.display();
+
+  detectcollision(stone, mango1);
+  detectcollision(stone, mango2);
+  detectcollision(stone, mango3);
+  detectcollision(stone, mango4);
+  detectcollision(stone, mango5);
+  detectcollision(stone, mango6);
 
 
-
-
+ 
 }
+
+function mouseDragged()
+{
+	Matter.Body.setPosition(stone.body, {x:mouseX, y:mouseY}) 
+}
+
+function mouseReleased()
+{
+	slingShot.fly();
+}
+
+function keyPressed() {
+	if (keyCode === 32) {
+    Matter.Body.setPosition(stone.body, {x:235, y:420}) 
+	slingShot.attach(stone.body);
+	}
+  }
+
+function detectcollision(lstone,lmango){
+
+    mangoBodyPosition=lmango.body.position
+    stoneBodyPosition=lstone.body.position
+    
+    var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
+      if(distance<=lmango.r+lstone.r)
+      {
+        Matter.Body.setStatic(lmango.body,false);
+      }
+  
+    }
+
+
+
